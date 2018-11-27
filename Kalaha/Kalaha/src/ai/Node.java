f08fc19ec0ccb38d3dd04daaa044bc045ea0929f
Kundan Reddy <32237665+kundanreddy@users.noreply.github.com>
2018-11-27 18:28:15 +0100

    Kalaha

---
+++ Kalaha/Kalaha/src/ai/Node.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ai;
+
+/**
+ *
+ * @author kundan
+ */
+public class Node {
+    
+    //utility value
+    int utility = -999;
+    //ambos
+    int ambos = 0;
+}
+
